a40743b7ba01733bb4a51a0d70d9af68766f4b24
sapna0994 <chouhan.sapna192@gmail.com>
2021-04-28 16:51:03 +0530

    new task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,37 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatListModule} from '@angular/material/list';
+import {MatDialogModule} from '@angular/material/dialog';
+import {LayoutModule} from '@angular/cdk/layout';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NgbModule,
+    MatDialogModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,66 @@
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {MatSidenav} from '@angular/material';
+import {Observable} from 'rxjs';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import {map} from 'rxjs/operators';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
+})
+export class MenuComponent implements OnInit {
+@ViewChild('drawer', {static: false}) sidenav: MatSidenav;
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(map(result => result.matches));
+  isHandsetLandscape$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.HandsetLandscape)
+    .pipe(map(result => result.matches));
+  isMedium$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Medium)
+    .pipe(map(result => result.matches));
+  isTablet$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Tablet)
+    .pipe(map(result => result.matches));
+
+  addItemPage = false;
+  itemListPage = false;
+
+  links = [
+    {name: 'Add Item', link: 'add_item', cls: 'new'},
+    {name: 'Item List', link: 'item_list', cls: 'list'},
+  
+  ];
+
+  constructor(
+  	private breakpointObserver: BreakpointObserver,
+    private route: Router
+  ) { 
+	if (this.route.url === '/add_item') {
+	      this.addItemPage = true;
+	      this.itemListPage = false; 
+	    } else if (this.route.url === '/item-list') {
+	      this.itemListPage = false;
+	      this.addItemPage = true;  
+	    }
+	}
+
+  ngOnInit(): void {
+  }
+checkNavigation(cls) {
+    if (cls === 'newClass') {
+        this.addItemPage = true;
+	    this.itemListPage = false; 
+    } else if (cls === 'listClass') {
+        this.itemListPage = false;
+	    this.addItemPage = true; 
+    } 
+  }
+logout() {
+    localStorage.clear();
+    location.href = '/login';
+  }
+
+}
